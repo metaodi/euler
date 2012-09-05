d5659a1ba02eed089b92602384696923f93a8841
Stefan Oderbolz <oderbolz@gmail.com>
2012-09-05 16:50:32 +0200

    Solved problem 8 (greatest product of five consecutive digits in a 1000-digit number)

--- problem8/greatest_product_finder.rb
+++ problem8/greatest_product_finder.rb
@@ -1,3 +1,35 @@
 class GreatestProductFinder
   # Find the greatest product of five consecutive digits in the 1000-digit number (see input.txt)
+
+  def number_stream
+    File.open("input.txt", "r") do |infile|
+      while (line = infile.gets)
+        line.each_char { |digit| yield Integer(digit) if digit =~ /\d/ }
+      end
+    end
+  end
+
+  def number_block_stream(block_size)
+    block = []
+    number_stream do |digit|
+      if block.size == block_size
+        yield block
+        block.shift
+      end
+      block << digit
+    end
+  end
+
+  def find_greatest_product
+    max_product = 0
+    number_block_stream(5) do |block|
+      product = block.reduce(:*)
+      max_product = product if product > max_product
+    end
+    max_product
+  end
+
 end
+
+product_finder = GreatestProductFinder.new
+puts "Greatest product: #{product_finder.find_greatest_product}"
--- problem8/test_greatest_product_finder.rb
+++ problem8/test_greatest_product_finder.rb
@@ -18,7 +18,32 @@ class TestGreatestProductFinder < MiniTest::Unit::TestCase
     assert_kind_of(GreatestProductFinder, @finder)
   end
 
-  def test_number_stream
-    fail("not yet implemented")
+  def test_number_stream_first
+    result = []
+    @finder.number_stream do |digit|
+      result << digit
+      break if result.size == 5
+    end
+    assert_equal([7,3,1,6,7], result)
+  end
+
+  def test_number_stream_size
+    result = []
+    @finder.number_stream { |digit| result << digit }
+    assert_equal(1000, result.size)
+  end
+
+  def test_number_block_stream
+    @finder.number_block_stream(2) do  |block|
+      assert_equal(2,block.size)
+      assert_kind_of(Integer,block[0])
+      assert_kind_of(Integer,block[1])
+    end
+  end
+
+  def test_find_greatest_product
+    product = @finder.find_greatest_product
+    refute_equal(0,product)
+    assert_kind_of(Integer,product)
   end
 end
